38a039461d73dae61a7c6d349a564b5ddca02278
old789 <old789@users.noreply.github.com>
2023-05-13 15:05:10 +0300

    begin adding 3 color LED

--- blink_3_color.ino
+++ blink_3_color.ino
@@ -1,10 +1,34 @@
+
+#define		LED_RED		9
+#define		LED_GREEN	10
+#define		LED_BLUE	11
+
+#define		MAIN_DELAY		2000
+
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
 }
+
+uint8_t		i = 0;
 
 void loop()
 {
   digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN) ^ HIGH);
-  delay(2000);
+  if ( i == 0 ) {
+		digitalWrite( LED_RED, HIGH );
+		digitalWrite( LED_GREEN, HIGH );
+		digitalWrite( LED_BLUE, HIGH );
+		delay(MAIN_DELAY);
+		digitalWrite( LED_RED, HIGH );
+		digitalWrite( LED_GREEN, HIGH );
+		digitalWrite( LED_BLUE, HIGH );
+	} else {
+		delay(MAIN_DELAY);
+	}
+	if ( ++i > 3 ) i = 0;
+	delay(MAIN_DELAY);
 }
