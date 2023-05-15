d156f9ed5514658f4d37a8abd897a59e14e85b27
old789 <old789@users.noreply.github.com>
2023-05-15 10:14:08 +0300

    Tabs had replaced with spaces

--- blink_3_color.ino
+++ blink_3_color.ino
@@ -1,33 +1,31 @@
 
-#define		LED_RED		9
-#define		LED_GREEN	10
-#define		LED_BLUE	11
+#define   LED_RED   9
+#define   LED_GREEN 10
+#define   LED_BLUE  11
 
-#define		MAIN_DELAY		2000
+#define   MAIN_DELAY    2000
 
-void setup()
-{
+void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);
   pinMode(LED_BLUE, OUTPUT);
 }
 
-uint8_t		i = 0;
-uint8_t		state = LOW;
+uint8_t   i = 0;
+uint8_t   state = LOW;
 
-void loop()
-{
-	state ^= HIGH;
-  digitalWrite(LED_BUILTIN, state );
+void loop() {
+  state ^= HIGH;
+  digitalWrite( LED_BUILTIN, state );
   if ( i == 0 ) {
-		for ( uint8_t j = 0; ( j + LED_RED ) <= LED_BLUE; j++ ){
-			digitalWrite( j, state );
-		}
-	} else {
-		digitalWrite( ( i + LED_RED - 1 ), state );
-	}
-	if ( state == LOW ) i++;
-	if ( i > 3 ) i = 0;
-	delay(MAIN_DELAY);
+    for ( uint8_t j = 0; ( j + LED_RED ) <= LED_BLUE; j++ ) {
+      digitalWrite( j, state );
+    }
+  } else {
+    digitalWrite( ( i + LED_RED - 1 ), state );
+  }
+  if ( state == LOW ) i++;
+  if ( i > 3 ) i = 0;
+  delay(MAIN_DELAY);
 }
