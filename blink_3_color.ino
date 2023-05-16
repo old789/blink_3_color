08ff5f1f1354e2d7e05b8b69e2ed72f1677ddf74
old789 <old789@users.noreply.github.com>
2023-05-16 10:51:54 +0300

    Update blink_3_color.ino
    New blinking mode - all colors in turn

--- blink_3_color.ino
+++ blink_3_color.ino
@@ -12,10 +12,22 @@ void setup() {
   pinMode(LED_BLUE, OUTPUT);
 }
 
+uint8_t   mode = 1;
+
 uint8_t   i = 0;
 uint8_t   state = LOW;
 
 void loop() {
+  switch (mode) {
+    case 1:
+      all_colors_in_turn();
+      break;
+    default:
+      blink_4_color();
+  }
+}
+
+void blink_4_color() {
   state ^= HIGH;
   digitalWrite( LED_BUILTIN, state );
   if ( i == 0 ) {
@@ -32,4 +44,21 @@ void loop() {
     }
   }
   delay(MAIN_DELAY);
+}
+
+void all_colors_in_turn() {
+  uint8_t kl = 0;
+  
+  for ( uint8_t j = 0; j < 3 ; j++ ) {
+    kl = ( state >> j ) & 1;
+    digitalWrite( ( j + LED_RED ), kl );
+  }
+
+  state++;
+  state &= 7;
+
+  for ( uint8_t j = 0; j < 2 ; j++ ) {
+    digitalWrite( LED_BUILTIN, digitalRead(LED_BUILTIN) ^ HIGH );
+    delay(MAIN_DELAY / 2);
+  }
 }
